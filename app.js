5089ee8c2d49ea5cda92fd3a6cdc5102e5a1ab55
Chris Hunley <cshunley21@gmail.com>
2021-11-02 09:05:48 -0500

    fixed errors

--- app.js
+++ app.js
@@ -8,25 +8,49 @@ setTimeout(() => {
   
 // 2
 setTimeout(() => {
-    const nesting = document.querySelector("#nesting");
-    const p2 = document.createElement("p");
-    p2.innerText = "Outer";
-    nesting.append(p2);
+    const div2 = document.querySelector(`#timeout-nesting`);
+    const p2 = document.createElement(`p`);
+    p2.innerText = `One`;
+    div2.append(p2);
+    // Nested setTimeout
     setTimeout(() => {
-      const p3 = document.createElement("p");
-      p3.innerText = "Inner";
-      nesting.append(p3);
+      const p3 = document.createElement(`p`);
+      p3.innerText = `Two`;
+      div2.append(p3);
     }, 1000);
   }, 2000);
-
+  
 // 3
-const count = document.querySelector("#count");
-const num = document.createElement("h1");
-for (i = 10; i >= 0; i++) {
-    (function(i){
-  setTimeout(() => {
-    num.innerText = `${i}`;
-    count.append(num);
-  }, 1000*(i+1));
-})(i);
-}
+  let counter = 1;
+  let time = setInterval(() => {
+    console.log(counter);
+    counter++;
+  }, 1000);
+
+  const stop = document.querySelector(`button`);
+  stop.addEventListener(`click`, () => {
+    clearInterval(time);
+  });
+
+  // 4
+  const countdown = document.querySelector(`#countdown`);
+  const p4 = document.createElement(`p`);
+  p4.innerHTML = `2:00`;
+  countdown.append(p4);
+  
+  const startTime = 2;
+  let time1 = startTime * 60;
+  function updateCountdown(){
+    const minutes = Math.floor(time / 60);
+    let seconds = time1 % 60;
+    seconds = seconds < 10 ? `0` + seconds : seconds;
+    countdown.innerHTML = `${minutes} : ${seconds}`;
+    time--;
+  }
+  
+  setInterval(() => {
+    updateCountdown();
+    if (time1 <= 0){
+      countdown.innerHTML = `TIME IS UP`;
+    }
+  }, 1000);
